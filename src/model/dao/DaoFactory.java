fdeb62d3261dcdf28d6777b3e32f8af6e7d6603d
klvo <klvo.gtfo.gmail.com>
2020-08-25 16:25:10 -0300

    SellerDaoJDBC and DAOFactory

---
+++ src/model/dao/DaoFactory.java
@@ -0,0 +1,11 @@
+package model.dao;
+
+import model.dao.impl.SellerDaoJDBC;
+
+public class DaoFactory {
+	
+	public static SellerDao createSellerDao() { // classe vai expor um metodo que retorna o tipo da interface no caso (SellerDao)
+		return new SellerDaoJDBC();				// Mas internamente ela vai instanciar uma implementação. Macete para não precisar expor a implementação e sim somente a interface
+	}				
+			// no programa principal sera ex de instancçao "SellerDao sellerDao = DaoFactory.createSellerDao();"		
+}			// Dessa forma o programa não conhece a implementação e sim somente a interface, é tambem uma forma de fazer injeção de dependencia sem explicitar a implementação
